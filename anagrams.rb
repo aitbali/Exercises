6cb6cf222100f1c70618167cb943750b1018b1b8
Abderrahim <aitbali@hotmail.com>
2019-06-26 14:10:29 +0200

    add anagrams algorithm

--- anagrams.rb
+++ anagrams.rb
@@ -1,26 +1,44 @@
-def anagram(string1,string2)
-    arr1 = []
-    arr2 = []
+# frozen_string_literal: true
 
-    string1.split("").each do |letter|
-        arr1 << letter.downcase 
-    end
+@arr1 = []
+@arr2 = []
+def clean(st1, st2)
+  st1.delete(' ').split('').each do |letter|
+    @arr1 << letter.downcase
+  end
 
-    string2.split("").each do |letter|
-        arr2 << letter.downcase 
-    end
-
-    arr1.each do |letter| 
-        if arr2.include?(letter)
-            arr2.delete(letter)
-        end           
-    end
-
-    if arr2.length == 0
-        return true
-    else 
-        return false
-    end
+  st2.delete(' ').split('').each do |letter|
+    @arr2 << letter.downcase
+  end
 end
 
-p anagram('dog','god')
+def anagram(st1, st2)
+  clean(st1, st2)
+
+  @arr1.each do |l1|
+    n = 0
+    m = 0
+    @arr2.each do |l2|
+      if l1 == l2
+        @arr2.delete_at(n)
+        m = 1
+        break
+      else
+        n += 1
+      end
+    end
+    return false if m.zero?
+  end
+
+  left(@arr2)
+end
+
+def left(array)
+  if array.empty?
+    true
+  else
+    false
+  end
+end
+
+p anagram('d o       g', 'g    d o')
